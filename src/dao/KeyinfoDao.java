d075c3751577cfde9e0aa12bc8c870eb6126e857
kuchiki111 <394957249@qq.com>
2018-01-25 08:54:29 +0800

    2018 01 28
    暂时上传

--- src/dao/KeyinfoDao.java
+++ src/dao/KeyinfoDao.java
@@ -1,4 +1,15 @@
 package dao;
 
+import javax.xml.crypto.dsig.keyinfo.KeyInfo;
+import java.util.List;
+
 public interface KeyinfoDao {
+    List<KeyInfo> showRecords();
+
+    KeyInfo findRecordsById(long id);
+    KeyInfo findRecordsByKey(String Key);
+
+    boolean update(KeyInfo keyInfo);
+    boolean delete(KeyInfo keyInfo);
+    boolean inseert(KeyInfo keyInfo);
 }
--- src/dao/impl/KeyinfoImpl.java
+++ src/dao/impl/KeyinfoImpl.java
@@ -1,4 +1,41 @@
 package dao.impl;
 
-public class KeyinfoImpl {
+import dao.KeyinfoDao;
+import org.springframework.orm.hibernate5.support.HibernateDaoSupport;
+
+import javax.xml.crypto.dsig.keyinfo.KeyInfo;
+import java.util.List;
+
+public class KeyinfoImpl  extends HibernateDaoSupport implements KeyinfoDao {
+    @Override
+    public List<KeyInfo> showRecords() {
+        String hql = "from Keyconfirm";
+        List<KeyInfo> list = (List<KeyInfo>) this.getHibernateTemplate().find(hql);
+        return list;
+    }
+
+    @Override
+    public KeyInfo findRecordsById(long id) {
+        return null;
+    }
+
+    @Override
+    public KeyInfo findRecordsByKey(String Key) {
+        return null;
+    }
+
+    @Override
+    public boolean update(KeyInfo keyInfo) {
+        return false;
+    }
+
+    @Override
+    public boolean delete(KeyInfo keyInfo) {
+        return false;
+    }
+
+    @Override
+    public boolean inseert(KeyInfo keyInfo) {
+        return false;
+    }
 }
